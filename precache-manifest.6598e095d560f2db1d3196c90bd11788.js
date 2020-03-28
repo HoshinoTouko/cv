78c35817fdee3869128050c9ec89f763a5bb34e3
HoshinoTouko <oranly@hotmail.com>
2020-03-29 00:14:12 +0800

    Initial commit.

---
+++ precache-manifest.6598e095d560f2db1d3196c90bd11788.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c94758512337ad36a515a1b523a26a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6275d2053d098a4d36c",
+    "url": "./static/css/2.db3c51a4.chunk.css"
+  },
+  {
+    "revision": "02afe0be5b73edcc6e7c",
+    "url": "./static/css/main.77c0e908.chunk.css"
+  },
+  {
+    "revision": "f6275d2053d098a4d36c",
+    "url": "./static/js/2.8bd00779.chunk.js"
+  },
+  {
+    "revision": "f4f32d8b5b3a58c4d0eb5daa0f0063f9",
+    "url": "./static/js/2.8bd00779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02afe0be5b73edcc6e7c",
+    "url": "./static/js/main.5bc9ce84.chunk.js"
+  },
+  {
+    "revision": "1dadb82055d25b5da444",
+    "url": "./static/js/runtime-main.8c907a67.js"
+  },
+  {
+    "revision": "e34e20dd2fb18a6d6ee87c58bfc0e1a9",
+    "url": "./static/media/avatar.e34e20dd.png"
+  }
+]);
